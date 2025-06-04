aa380f6e9d87340225508785f28ef78d28755238
amit-3012003 <144511286+amit-3012003@users.noreply.github.com>
2025-06-04 15:41:48 +0000

    Added Hello, React! component to Ques_1.jsx

--- src/Module 3/Introduction to ReactJS/Ques_1.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_1.jsx
@@ -1,14 +1,16 @@
 // JSX Syntax Basics
 // Description: Write basic JSX that displays "Hello, React!"
 // Use this component in App.js
+// Ques_1.jsx
+// This component displays a simple greeting using JSX. It is a basic example for understanding React functional components.
+
 import React from 'react';
 
-function Ques_1() {
-  return (
-    <h1>Hello, React!</h1>
-  );
-}
+const Ques_1 = () => {
+  return <h1>Hello, React!</h1>;
+};
 
-export default Ques_1;
+export default React.memo(Ques_1);  // Optional: to optimize performance by memoizing
+
 
 
